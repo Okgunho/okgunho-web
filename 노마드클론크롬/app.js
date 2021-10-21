ec25018164421ca923b9f7ba9fc47a701977cd4f
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-10-21 19:04:32 +0900

    10/21

--- 노마드클론크롬/app.js
+++ 노마드클론크롬/app.js
@@ -1,4 +1,14 @@
-const amIFat = null;
-let something;
-console.log(amIFat)
-console.log(something, amIFat)
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sund";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "gunho"];
+
+console.log(daysOfWeek)
+console.log(nonsense)
