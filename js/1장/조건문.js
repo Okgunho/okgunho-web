88e48492bd8b83ace935531812a9c20d4a907f82
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-10-28 19:42:39 +0900

    10/28

--- js/1장/조건문.js
+++ js/1장/조건문.js
@@ -16,3 +16,49 @@ if (true) {
     console.log('if문 안의 a 값은 ' + a); //2
 }
 console.log('if문 밖의 a 값은 ' + a);   //1
+
+//-------------------------------------------------------
+
+//if - else 문
+
+const a = 10;
+if (a > 15) {
+    console.log('a 가 15 큽니다.');
+} else {
+    console.log('a 가 15보다 크지 않습니다.');
+}
+// a가 15보다 크지 않습니다.
+
+//--------------------------------------------------------
+
+//if - else if 문
+
+const a = 10;
+if (a === 5) {
+    console.log('5입니다!');
+} else if (a === 10) {
+    console.log('10입니다!');
+} else {
+    console.log('5도 아니고 10도 아닙니다.');
+}
+// 10입니다!
+
+// -------------------------------------------------------
+
+// swich case 문
+
+const device = 'iphone';
+
+switch (device) {
+    case 'iphone':
+        console.log('아이폰!');
+        break;
+    case 'ipad':
+        console.log('아이패드!');
+        break;
+    case 'galaxy note':
+        console.log('갤럭시 노트!');
+        break;
+    default:
+        console.log('모르겠네요..');
+}
