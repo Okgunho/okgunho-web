15ed537db8624488af0951e9bbfda78a9e41f792
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-11-01 15:18:17 +0900

    11/01

--- js/1장/객체.js
+++ js/1장/객체.js
@@ -38,3 +38,82 @@ const captainAmerica = {
 
 console.log(ironMan);
 console.log(captainAmerica);
+
+//------------------------------
+
+// 함수에서 객체를 파라미터로 받기
+
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+};
+
+const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+};
+
+function print(hero) {
+    const text = `${hero.alias}(${hero.name}) 역할을 맡은 배우는 ${hero.actor                                             //템플릿 리터럴 
+        } 입니다.`;
+    console.log(text);
+}
+
+print(ironMan);
+print(captainAmerica);
+//아이언맨(토니 스타크) 역할을 맡은 배우는 로버트 다우니 주니어 입니다.
+//캡틴 아메리카(스티븐 로저스) 역할을 맡은 배우는 크리스 에반스 입니다.
+
+//---------------------------------
+
+// 객체 비구조화 할당
+// 이 문법은 "객체 구조 분해" 라고 불리기도 합니다.
+
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+};
+
+const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+};
+
+function print(hero) {
+    const { alias, name, actor } = hero;
+    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
+    console.log(text);
+}
+
+print(ironMan);
+print(captainAmerica);
+
+
+//더 깔끔하게 
+
+
+const ironMan = {
+    name: '토니 스타크',
+    actor: '로버트 다우니 주니어',
+    alias: '아이언맨'
+};
+
+const captainAmerica = {
+    name: '스티븐 로저스',
+    actor: '크리스 에반스',
+    alias: '캡틴 아메리카'
+};
+
+function print({ alias, name, actor }) {
+    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
+    console.log(text);
+}
+
+print(ironMan);
+print(captainAmerica);
+
+  //--------------------------------
