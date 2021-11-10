5c77761f28ef50dd00b4ce02ff73befcf73876b2
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-11-10 14:23:34 +0900

    11/10

--- js/1장/반복문.js
+++ js/1장/반복문.js
@@ -49,3 +49,61 @@ const doggy = {
 console.log(Object.entries(doggy));
 console.log(Object.keys(doggy));
 console.log(Object.values(doggy));
+
+// Object.entries: [[키, 값], [키, 값]] 형태의 배열로 변환
+// Object.keys: [키, 키, 키] 형태의 배열로 변환
+// Object.values: [값, 값, 값] 형태의 배열로 변환
+
+// 객체가 지니고 있는 값에 대하여 반복을 하고 싶다면 
+// 위 함수들을 사용해도 되고, for...in 구문을 사용해도 된다.
+
+const doggy = {
+    name: '멍멍이',
+    sound: '멍멍',
+    age: 2
+};
+
+for (let key in doggy) {
+    console.log(`${key}: ${doggy[key]}`);
+}
+
+//----------------------------------------
+
+for (let i = 0; i < 10; i++) {
+    if (i === 2) continue; // 다음 루프를 실행
+    console.log(i);
+    if (i === 5) break; // 반복문을 끝내기
+}
+
+//-----------------------------------------
+
+function sumOf(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result);
+
+//15
+
+//-------------------------------------------
+
+//퀴즈
+
+function biggerThanThree(numbers) {
+    const p = [];
+    for (let i = 0; i < numbers.length; i++)
+        if (numbers[i] > 3) {
+            p.push(numbers[i]);
+        }
+
+    return p;
+
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(numbers)); // [4, 5, 6, 7]
