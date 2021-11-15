ab93070077fbde7fe1572e93c8371c66ea2a2d37
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-11-15 18:00:14 +0900

    11/15

--- js/1장/배열내장함수.js
+++ js/1장/배열내장함수.js
@@ -60,3 +60,143 @@ console.log(squared);
 //-----------------------------------
 
 // indexOf
+
+const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
+const index = superheroes.indexOf('토르');
+console.log(index); // 2
+
+// 토르가 몇 번째 항복인지 알고 싶을 때
+
+//-------------------------------------
+
+// findindex
+
+// 예를 들어서 다음과 같은 배열이 있다고 가정해봅시다.
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+];
+
+const index = todos.findIndex(todo => todo.id === 3);
+console.log(index);
+
+// 여기서 만약 id 가 3 인 객체가 몇번째인지 찾으러면, findIndex 
+// 함수에 검사하고자 하는 조건을 반환하는 함수를 넣어서 찾을 수 있다.
+
+// 결과는 2
+
+//------------------------------------
+
+// find
+
+// find 함수는 findIndex 랑 비슷한데, 찾아낸 값이 몇번째인지 
+// 알아내는 것이 아니라, 찾아낸 값 자체를 반환한다.
+
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+];
+
+const todo = todos.find(todo => todo.id === 3);
+console.log(todo);
+
+// {id: 3, text: "객체와 배열 배우기", done: true}
+
+//-------------------------------------------
+
+// filter
+
+// filter 함수는 배열에서 특정 조건을 만족하는 값들만 따로 추출하여 새로운 배열을 만든다. 
+// 예를 들어서, 우리가 방금 만들었던 todos 배열에서 done 값이 false 인 항목들만 따로 
+// 추출해서 새로운 배열을 만든다.
+
+const todos = [
+    {
+        id: 1,
+        text: '자바스크립트 입문',
+        done: true
+    },
+    {
+        id: 2,
+        text: '함수 배우기',
+        done: true
+    },
+    {
+        id: 3,
+        text: '객체와 배열 배우기',
+        done: true
+    },
+    {
+        id: 4,
+        text: '배열 내장함수 배우기',
+        done: false
+    }
+];
+
+// const tasksNotDone = todos.filter(todo => !todo.done);
+const tasksNotDone = todos.filter(todo => todo.done === false);
+console.log(tasksNotDone);
+
+//결과 값
+
+// [
+//     {
+//       id: 4,
+//       text: '배열 내장 함수 배우기',
+//       done: false
+//     }
+//   ];
+
+//--------------------------------
+
+// splice
+
+// 배열에서 특정 항목을 지울 때 사용.
+
+const numbers = [10, 20, 30, 40];
+const index = numbers.indexOf(30);
+numbers.splice(index, 1);
+console.log(numbers);
+
+// 결과 [ 10, 20, 40 ]
+
+//-------------------------------------
+
+// slice
+//
