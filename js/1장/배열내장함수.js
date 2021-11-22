377c3c12f68863a109028a7a5ea0eefa5da77ac0
옥건호 <82382500+Okgunho@users.noreply.github.com>
2021-11-22 12:10:16 +0900

    11/22

--- js/1장/배열내장함수.js
+++ js/1장/배열내장함수.js
@@ -199,4 +199,73 @@ console.log(numbers);
 //-------------------------------------
 
 // slice
-//
+// slice 는 splice 랑 조금 비슷한데, 배열을 잘라낼 때 사용한다. 
+// 중요한 점은 기존의 배열은 건들이지 않는 다는 것이다.
+
+
+const numbers = [10, 20, 30, 40];
+const sliced = numbers.slice(0, 2); // 0부터 시작해서 2전까지
+
+console.log(sliced); // [10, 20]
+console.log(numbers); // [10, 20, 30, 40]
+
+//---------------------------------------
+
+// shift 와  pop
+
+
+//shift
+// shift 는 첫번째 원소를 배열에서 추출해줍니다. 
+// (추출하는 과정에서 배열에서 해당 원소는 사라집니다.)
+const numbers = [10, 20, 30, 40];
+const value = numbers.shift();
+console.log(value);
+console.log(numbers);
+
+
+// pop 
+// pop 은 push 의 반대로 생각하시면 됩니다. 
+// push 는 배열의 맨 마지막에 새 항목을 추가하고, 
+// pop 은 맨 마지막 항목을 추출합니다.
+
+const numbers = [10, 20, 30, 40];
+const value = numbers.pop();
+console.log(value);
+console.log(numbers);
+
+//-----------------------------------
+
+// unshift
+// shift의 반대
+// 배열의 맨 앞에 새 원소를 추가합니다.
+
+const numbers = [10, 20, 30, 40];
+numbers.unshift(5);
+console.log(numbers);
+
+//--------------------------------------
+
+// concat 
+// concat 은 여러개의 배열을 하나의 배열로 합쳐줍니다
+// concat 함수는 arr1 과 arr2 에 변화를 주지 않습니다.
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const concated = arr1.concat(arr2);
+
+console.log(concated);
+
+//------------------------------------
+
+// join
+//join 은 배열 안의 값들을 문자열 형태로 합쳐줍니다.
+
+const array = [1, 2, 3, 4, 5];
+console.log(array.join()); // 1,2,3,4,5
+console.log(array.join(' ')); // 1 2 3 4 5
+console.log(array.join(', ')); // 1, 2, 3, 4, 5
+
+//------------------------------------
+
+// reduce
+
